7c88a77f945811c3fdb08622436c6ea5db96b9d8
chinxeleer <blessingeleer.programming@gmail.com>
2024-12-05 16:19:31 +0200

    Styles the blogcards that I think were causing hydration bugs

--- src/blog/blog_card.rs
+++ src/blog/blog_card.rs
@@ -10,10 +10,10 @@ pub fn BlogCard(
 ) -> impl IntoView {
     view! {
         <A href=link class="text-purple-300">
-            <div class="w-[400px] md:w-[600px] flex flex-col rounded-lg shadow-sm p-4 md:p-5 bg-slate-900 border border-rose-300">
+            <div class="w-[300px] md:w-[600px] flex flex-col rounded-lg shadow-sm p-4 md:p-5 bg-slate-900 border border-rose-300">
                 <h3 class="text-lg font-bold">{title}</h3>
                 <p class="mt-1 text-xs uppercase text-rose-100 justify-end">{date}</p>
-                <div class="flex space-x-2 mt-2">
+                <div class="flex overflow-auto space-x-2 mt-2">
                     {tags
                         .unwrap_or_default()
                         .into_iter()
@@ -28,8 +28,8 @@ pub fn BlogCard(
 #[component]
 fn CardItem(title: String) -> impl IntoView {
     view! {
-        <div class="rounded-xl bg-pink-200 px-2">
-            <p class="text-sm text-purple-700 font-bold">{title}</p>
+        <div class="rounded-xl bg-pink-200 px-1">
+            <p class="text-xs md:text-sm text-purple-700 font-bold">{title}</p>
         </div>
     }
 }
